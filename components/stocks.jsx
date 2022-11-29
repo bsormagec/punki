986366ad8e2f45f8176c920468f887c6405756dc
Pagzi Tech Inc <91607263+PagziTechInc@users.noreply.github.com>
2022-11-29 02:04:41 -0500

    Update stocks.jsx

--- components/stocks.jsx
+++ components/stocks.jsx
@@ -5,7 +5,7 @@ const Stocks = ({ ship, currentIsland, warehouse, maxStorage, currentCargoWeight
   return (
     <div style={{ display: 'flex', width: 'fit-content' }}>
       <div className="panel">
-        <h3>{`Ship ${currentCargoWeight}/${maxStorage}kg`}</h3>
+        <h3>{`Vehicle ${currentCargoWeight}/${maxStorage}kg`}</h3>
         {Object.keys(ship.storage.contents).map((goodType, i) => {
           return (
             <div key={i} style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -17,7 +17,7 @@ const Stocks = ({ ship, currentIsland, warehouse, maxStorage, currentCargoWeight
       </div>
 
       <div className="panel">
-        <h3>{'Market Prices'}</h3>
+        <h3>{'Town Prices'}</h3>
         {Object.keys(currentIsland.market).map((goodType, i) => {
           return (
             <div key={i} style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -29,7 +29,7 @@ const Stocks = ({ ship, currentIsland, warehouse, maxStorage, currentCargoWeight
       </div>
 
       <div className="panel">
-        <h3>{'Warehouse Stocks'}</h3>
+        <h3>{'Hometown Stocks'}</h3>
         {Object.keys(warehouse.contents).map((goodType, i) => {
           return (
             <div key={i} style={{ display: 'flex', justifyContent: 'space-between' }}>
