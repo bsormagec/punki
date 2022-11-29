791a4b5fbf7b85b0bf7d221623deee291047fd0c
Pagzi Tech Inc <91607263+PagziTechInc@users.noreply.github.com>
2022-11-29 02:00:26 -0500

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,38 +6,37 @@ export default () => {
   return (
     <div style={{ fontFamily: 'monospace' }}>
       <div style={{ width: '600px' }}>
-        <h1>{'Taipan Punks'}</h1>
-        <h3>{'A Taipan-like game of profit and adventure'}</h3>
+        <h1>{'Nomadic Punks Trader'}</h1>
+        <h3>{'A Taipan-like game of profit and adventure in a nomadic world of tribes.'}</h3>
         <p>
           {`
-            It is Spring, 1698.
-            You are a small-time coureur-des-bois plying the Saint Lawrence River and Great Lakes in search of a fast buck.
-            It's miserable business but somebody has to do it.
-            Besides, there's a fortune to be made.
+            It is Spring, 2023.
+            You belong to a nomadic tribe.
+            You travel, trade and profit.
+            And there's a fortune to be made.
           `}
         </p>
 
         <p>
           {`
-            Luckily you are of a hardy constitution and you are more than willing to do what it takes to get deals done.
-            You have no qualms about buying and selling questionable goods to the highest bidder.
+            Prove your trading skills in a dynamic market.
           `}
         </p>
 
         <p style={{ marginBottom: '20px' }}>
           {`
-            But it's dangerous out there in the wilderness and you have enemies everywhere.
-
-            You've got one year to prove your worth. Everyone is eager to see how you will do.
+            But it's dangerous out there in the wilderness and you have rivals.
+            
+            Who are more than willing to fight for the same fortune.
           `}
         </p>
 
-        <Link href="/trader"><button>{'Click here to start'}</button></Link>
+        <Link href="/trader"><button>{'Play'}</button></Link>
 
 
 
           <div style={{ position:'absolute', bottom: '10px' }}>
-            {'Audio Credit: '}<a href="https://soundcloud.com/snkrthief" target="_blank" style={{ color: '#f3f3f3'}}>{'snkrthief'}</a>
+            {'Docs: '}<a href="https://github.com/PagziTechInc/punki/" target="_blank" style={{ color: '#f3f3f3'}}>{'docs'}</a>
           </div>
 
       </div>
