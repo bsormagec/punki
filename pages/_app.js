954dc7874fc460cfa6ea84bb68117000fcd4113a
PagziTechInc <ekimunal@pagzi.com>
2022-11-30 00:07:02 -0500

    Fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,27 @@
-import Head from 'next/head';
-import App, { Container } from 'next/app';
-import React from 'react';
-import withReduxStore from '../lib/with-redux-store';
-import { Provider } from 'react-redux';
+import Head from "next/head";
+import App, { Container } from "next/app";
+import React from "react";
+import withReduxStore from "../lib/with-redux-store";
+import { Provider } from "react-redux";
 
 class NomadicPunks extends App {
-  render () {
-    const { Component, pageProps, reduxStore } = this.props;
-    return (
-      <Container>
-        <Head>
-          <title>{'Nomadic Punks Trader'}</title>
-          <meta name="description" content="A Taipan-like game of profit and adventure in a nomadic world of tribes."/>
-        </Head>
-        <Provider store={reduxStore}>
-          <Component {...pageProps} />
-        </Provider>
-      </Container>
-    );
-  }
+    render() {
+        const { Component, pageProps, reduxStore } = this.props;
+        return (
+            <Container>
+                <Head>
+                    <title>{"Nomadic Punks Trader"}</title>
+                    <meta
+                        name="description"
+                        content="A Taipan-like game of profit and adventure in a nomadic world of tribes."
+                    />
+                </Head>
+                <Provider store={reduxStore}>
+                    <Component {...pageProps} />
+                </Provider>
+            </Container>
+        );
+    }
 }
 
 export default withReduxStore(NomadicPunks);
