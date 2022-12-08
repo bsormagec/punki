06695d94cf62fca56e15ac9a38238c07198cd01b
Burak Sormageç <bsormagec@users.noreply.github.com>
2022-12-08 16:28:36 +0300

    Fixed bank usage.

--- components/moneylender.jsx
+++ components/moneylender.jsx
@@ -1,51 +1,72 @@
 import React from 'react';
 import * as actions from '../lib/actions';
-import { connect } from 'react-redux';
-import { UI_SCENES } from '../lib/types';
+import {connect} from 'react-redux';
+import {UI_SCENES} from '../lib/types';
 
-const MoneyLender = ({ borrow, repay, setCurrentScene, cash, hasBorrowed }) => {
+const MoneyLender = ({borrow, repay, setCurrentScene, cash, hasBorrowed, loan}) => {
 
-  let textInput = React.createRef();
-  let repayInput = React.createRef();
+    let textInput = React.createRef();
+    let repayInput = React.createRef();
 
-  return (
-    <div style={{ padding: '10px', width: '320px', display: 'flex', flexDirection: 'column' }}>
-      <h3 style={{ display: 'flex', justifyContent: 'space-between' }}>
-        {'Loan Shark'}
-        <button onClick={setCurrentScene.bind(null, UI_SCENES.NULL)}>{'Back'}</button>
-      </h3>
-      {
-        !hasBorrowed ?
-        <>
-          <input
-            type="number"
-            style={{ marginBottom: '5px', marginRight: '5px', textAlign: 'center' }}
-            placeholder={`Borrow max ${Math.floor(cash * 2)}`}
-            ref={textInput}
-          />
-          <button onClick={() => borrow(parseInt(textInput.current.value, 10))}>{'Borrow'}</button>
-        </>
-        : null
-      }
+    return (
+        <div style={{padding: '10px', width: '320px', display: 'flex', flexDirection: 'column'}}>
+            <h3 style={{display: 'flex', justifyContent: 'space-between'}}>
+                {'Loan Shark'}
+                <button onClick={setCurrentScene.bind(null, UI_SCENES.NULL)}>{'Back'}</button>
+            </h3>
+            {
+                !hasBorrowed ?
+                    <>
+                        <div style={{
+                            'display': 'flex',
+                            'flexDirection': 'row',
+                            'width': '100%',
+                            'justifyContent': 'center',
+                            'alignItems': 'center',
+                            'marginBottom': '5px',
+                        }}
+                        >
+                            <input
+                                type="number"
+                                style={{marginRight: '5px', textAlign: 'center', width: '100%'}}
+                                placeholder={`Borrow max ${Math.floor(cash * 2)}`}
+                                onChange={() => textInput.current.value > Math.floor(cash * 2) ? textInput.current.value = Math.floor(cash * 2) : null}
+                                ref={textInput}
+                            />
+                            <button onClick={() => textInput.current.value = Math.floor(cash * 2)}>{'Max'}</button>
+                        </div>
 
-      <input
-        type="number"
-        style={{ marginBottom: '5px', marginRight: '5px', textAlign: 'center' }}
-        placeholder={'Repay how much?'}
-        ref={repayInput}
-      />
-      <button onClick={() => repay(parseInt(repayInput.current.value, 10))}>{'Repay'}</button>
-    </div>
-  );
+                        <button onClick={() => borrow(parseInt(textInput.current.value, 10))}>{'Borrow'}</button>
+                    </>
+                    : null
+            }
+
+            <input
+                type="number"
+                style={{marginBottom: '5px', marginRight: '5px', textAlign: 'center'}}
+                placeholder={'Repay how much?'}
+                hidden={loan < 1}
+                ref={repayInput}
+            />
+            <button hidden={loan < 1} onClick={() => repay(parseInt(repayInput.current.value, 10))}>{'Repay'}</button>
+        </div>
+    );
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  setCurrentScene: (sceneType) => dispatch(actions.setCurrentScene(sceneType)),
-  borrow: (amount) => dispatch(actions.borrow(amount)),
-  repay: (amount) => dispatch(actions.repay(amount))
+    setCurrentScene: (sceneType) => dispatch(actions.setCurrentScene(sceneType)),
+    borrow: (amount) => dispatch(actions.borrow(amount)),
+    repay: (amount) => {
+        if (amount > 0) {
+            dispatch(actions.repay(amount));
+        } else {
+            // eslint-disable-next-line no-alert
+            alert('You must repay at least 1');
+        }
+    }
 });
 
 export default connect(
-  (state) => ({ ...state }),
-  mapDispatchToProps
-  )(MoneyLender);
+    (state) => ({...state}),
+    mapDispatchToProps
+)(MoneyLender);
